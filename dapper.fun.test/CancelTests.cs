cf7debf7c4b2cd2593579584724cc942f52afa4e
Daniel <danieliozzi@gmail.com>
2019-10-28 07:06:10 +0200

    test cancel

---
+++ dapper.fun.test/CancelTests.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace dapper.fun.test
+{
+    using static dapper.fun.Selects;
+    using static dapper.fun.Connects;
+
+    [TestClass]
+    public class CancelTests
+    {
+        [TestMethod]
+        public async Task CancelsExec()
+        {
+            using (var con = Database.Connect())
+            {
+                using (var tokenSource = new CancellationTokenSource())
+                {
+                    var cancel = tokenSource.Token;
+                    var wait = Connected(Exec((text: "sqlilte3_sleep(100); select 1;", cancel)), con);
+                    tokenSource.Cancel();
+
+                    Exception err = null;
+                    try
+                    {
+                        await wait();
+                    }
+                    catch (Exception ex)
+                    {
+                        err = ex;
+                    }
+
+                    err.Should().NotBeNull();
+                    err.Should().BeOfType<TaskCanceledException>();
+                }
+
+            }
+        }
+        [TestMethod]
+        public async Task CancelsScalar()
+        {
+            using (var con = Database.Connect())
+            {
+                using (var tokenSource = new CancellationTokenSource())
+                {
+                    var cancel = tokenSource.Token;
+                    var wait = Connected(Scalar<int>((text: "sqlilte3_sleep(100); select 1;", cancel)), con);
+                    tokenSource.Cancel();
+
+                    Exception err = null;
+                    try
+                    {
+                        await wait();
+                    }
+                    catch (Exception ex)
+                    {
+                        err = ex;
+                    }
+
+                    err.Should().NotBeNull();
+                    err.Should().BeOfType<TaskCanceledException>();
+                }
+
+            }
+        }
+        [TestMethod]
+        public async Task CancelsQuery()
+        {
+            using (var con = Database.Connect())
+            {
+                using (var tokenSource = new CancellationTokenSource())
+                {
+                    var cancel = tokenSource.Token;
+                    var wait = Connected(Query<int>((text: "sqlilte3_sleep(100); select 1;", cancel)), con);
+                    tokenSource.Cancel();
+
+                    Exception err = null;
+                    try
+                    {
+                        await wait();
+                    }
+                    catch (Exception ex)
+                    {
+                        err = ex;
+                    }
+
+                    err.Should().NotBeNull();
+                    err.Should().BeOfType<TaskCanceledException>();
+                }
+
+            }
+        }
+    }
+}
